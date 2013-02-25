042ee07dc2bc0472ef50d1a5c2ee08c30568b3d9
Enrique <m4st3r321@yahoo.com>
2013-02-25 11:59:41 -0500

    Cleaned Version

--- server/chatRoom.js
+++ server/chatRoom.js
@@ -19,17 +19,6 @@ Meteor.users.allow({
 
 
 
-// Meteor.methods({
-//    addField: function () {
-//          console.log(Meteor.users.find().fetch());
-//    }
-//    // keepalive: function (user_id) {
-//    //       Players.update({_id: player_id},
-//    //                  {$set: {last_keepalive: (new Date()).getTime(),
-//    //                          idle: false}});
-//    // }
-
-// });
 
 Accounts.onCreateUser(function(options, user) {
   
@@ -42,17 +31,4 @@ Accounts.onCreateUser(function(options, user) {
   return user;
 });
 
-
-
-// Meteor.setInterval(function () {
-//   var now = (new Date()).getTime();
-//   var idle_threshold = now - 70*1000; // 70 sec
-//   var remove_threshold = now - 60*60*1000; // 1hr
-
-//   Players.update({last_keepalive: {$lt: idle_threshold}},
-//                  {$set: {idle: true}});
-
-//   // XXX need to deal with people coming back!
-//   // Players.remove({$lt: {last_keepalive: remove_threshold}});
-
-// }, 30*1000);
+ 

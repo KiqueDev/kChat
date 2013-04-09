4485e177989131a63176f697e61f9db75efb2fa8
Enrique <m4st3r321@yahoo.com>
2013-04-09 15:12:53 -0400

    Cleaner Version 2

--- client/kchat.js
+++ client/kchat.js
@@ -44,109 +44,69 @@ Template.lobby.rooms = function() {
 //EVENTS HANDLERS BUTTON
 Template.home.events({
   'click #chatnowBtn': function(evt) {
-    var user = $("#username").val().trim();
-    var password1 = $("#password1").val().trim();
-    var password2 = $("#password2").val().trim();
-    console.log(user);
-    if(user.length > 5 && user.length !== 0 && password1 === password2 && password1.length !=0){
-      console.log("passed");
-      Accounts.createUser({username:user, password:password1}, function(error){
-        if(error){
-          $("#errorUsernameMsg").text('User Already Exist');
-        }
-      });
-
-    }else{
-      $("#errorUsernameMsg").text('Username Must Contain At least 5 Characters');
-      //alert('Must Contain At least 5 Characters');
-    }
+    CreateUser();
   },
   'click #logoutBtn': function(evt) {
     Meteor.logout();
+  },
+  'keypress #formCreateUser': function(evt) {
+    if (evt.keyCode == 13){
+      CreateUser();
+      return false;
+    }
   }
 });
 
-//EVENTS HANDLERS BUTTON
-$(document).on("click", "#chatnowLoginBtn", function(event){
-    var username = $("#usernameLogin").val().trim();
-    var password = $("#passwordLogin").val().trim();
-    Meteor.loginWithPassword(username, password,
-      function (error){
-        if(error){
-          //alert("Failed to login");
-          $("#loginErroMsg").text('Warning: Incorrect Login');
-        }else{
-
-          // //PUT BOOLEAN FOR EACH OWNER ROOMS
-          // Meteor.call('SetupOwnerRooms');
-
-          $('#loginModal').modal('hide');
-          $('#usernameLogin').val('');
-          $('#passwordLogin').val('');
-          $("#loginErroMsg").text('');
-        }
-
-    });
-});
-
-$(document).on("click", "#joinRoom", function(event){
-  console.log('join');
-
-  var roomID= $(this).attr("roomId");
-  var roomTitle= $(this).attr("roomTitle");
-
-  //UPDATE USER WHEN JOINED ROOM
-  Meteor.call('UpdateUserRoomInfoToInside', roomID, roomTitle);
-});
-
-$(document).on("click", "#deleteRoom", function(event){
-  console.log('delete');
-
-  var roomID= $(this).attr("roomId");
-  console.log(roomID);
-  $(".deleteAlert-"+roomID).show('slow');
-});
-
-$(document).on("click", "#cancelDelete", function(event){
-  var roomID= $(this).attr("roomId");
-  console.log(roomID);
-  $(".deleteAlert-"+roomID).hide('slow');
-  //UPDATE USER WHEN JOINED ROOM
-  //Meteor.call('UpdateUserRoomInfoToInside', roomID, roomTitle);
-});
-
-$(document).on("click", "#deleteConfirm", function(event){
-  var roomID= $(this).attr("roomId");
-  console.log(roomID);
-  //DELETE ROOM
-  Meteor.call('DeleteRoom', roomID);
-});
-
-$(document).on("click", "#goout", function(event){
-  console.log("goout");
-
-  var roomID= $(this).attr("roomId");
-  //console.log(roomID);
-  Meteor.call('UpdateUserRoomInfoToOutside', roomID);
+Template.loginModal.events({
+  'click #chatnowLoginBtn': function(evt) {
+    LoginUser();
+  },
+  'keypress #formLoginUser': function(evt) {
+    if (evt.keyCode == 13){
+      LoginUser();
+      return false;
+    }
+  }
 });
 
 Template.lobby.events({
   'click #createRoomBtn': function(evt) {
-    var roomtitle = $("#roomTitleName").val().trim();
-    if(roomtitle.length !== 0 && roomtitle.length >= 4 && roomtitle.length <= 16 ){
-      Rooms.insert({ roomTitle:roomtitle, peopleID:[], peopleUsername:[], createdByID:Meteor.userId() });
-      $("#roomTitleName").val('');
-      $("#createRoomErroMsg").text('');
-    }else{
-      $("#createRoomErroMsg").text('Please Fill in Field (Must contain at LEAST 4 and at MOST 16 Characters');
+    CreateRoom();
+  },
+  'keypress #roomTitleName': function(evt) {
+    if (evt.keyCode == 13){
+      CreateRoom();
+      return false;
     }
+   },
+  'click .joinRoom': function(evt) {
+    var roomID= $(evt.target).attr("roomId");
+    var roomTitle= $(evt.target).attr("roomTitle");
+
+    //UPDATE USER WHEN JOINED ROOM
+    Meteor.call('UpdateUserRoomInfoToInside', roomID, roomTitle);
+  },
+  'click .deleteRoom': function(evt) {
+
+    var roomID= $(evt.target).attr("roomId");
+    $("#deleteAlert-"+roomID).show('slow');
+  },
+  'click .cancelDelete': function(evt) {
+    var roomID= $(evt.target).attr("roomId");
+    $("#deleteAlert-"+roomID).hide('slow');
+    //UPDATE USER WHEN JOINED ROOM
+    //Meteor.call('UpdateUserRoomInfoToInside', roomID, roomTitle);
+  },
+  'click .deleteConfirm': function(evt) {
+    var roomID= $(evt.target).attr("roomId");
+    //DELETE ROOM
+    Meteor.call('DeleteRoom', roomID);
   }
 });
 
 
 Template.room.events({
   'click #sendMessage': function(evt) {
-    console.log("send clicked");
     var msg= $("#messageTextArea").val();
 
     Messages.insert({
@@ -166,7 +126,6 @@ Template.room.events({
           return true;
       }
       else{
-        console.log("send clicked");
         var msg= $("#messageTextArea").val();
 
         Messages.insert({
@@ -181,10 +140,64 @@ Template.room.events({
       }
       return false;
     }
+  },  
+  'click #exitRoom': function(evt) {
+    var roomID= $(evt.target).attr("roomId");
+    Meteor.call('UpdateUserRoomInfoToOutside', roomID);
   }
 
 });
 
+//FUNCTIONS
+function CreateUser(){
+  var user = $("#username").val().trim();
+  var password1 = $("#password1").val().trim();
+  var password2 = $("#password2").val().trim();
+  if(user.length > 5 && user.length !== 0 && password1 === password2 && password1.length !=0){
+    Accounts.createUser({username:user, password:password1}, function(error){
+      if(error){
+        $("#errorUsernameMsg").text('User Already Exist');
+      }
+    });
+
+  }else{
+    $("#errorUsernameMsg").text('Username Must Contain At least 5 Characters');
+        //alert('Must Contain At least 5 Characters');
+    }
+}
+
+function LoginUser(){
+  var username = $("#usernameLogin").val().trim();
+  var password = $("#passwordLogin").val().trim();
+  Meteor.loginWithPassword(username, password,
+  function (error){
+    if(error){
+      //alert("Failed to login");
+      $("#loginErroMsg").text('Warning: Incorrect Login');
+    }else{
+
+      // //PUT BOOLEAN FOR EACH OWNER ROOMS
+      // Meteor.call('SetupOwnerRooms');
+
+      $('#loginModal').modal('hide');
+      $('#usernameLogin').val('');
+      $('#passwordLogin').val(''); 
+      $("#loginErroMsg").text('');
+    }
+
+  });
+}
+
+function CreateRoom(){
+  var roomtitle = $("#roomTitleName").val().trim();
+  if(roomtitle.length !== 0 && roomtitle.length >= 4 && roomtitle.length <= 16 ){
+    Rooms.insert({ roomTitle:roomtitle, peopleID:[], peopleUsername:[], createdByID:Meteor.userId() });
+    $("#roomTitleName").val('');
+    $("#createRoomErroMsg").text('');
+  }else{
+    $("#createRoomErroMsg").text('Please Fill in Field (Must contain at LEAST 4 and at MOST 16 Characters');
+  }
+}
 
 Meteor.startup(function() {
 
@@ -199,25 +212,20 @@ Meteor.autorun(function(){
 });
 
 Template.lobby.rendered = function(){
-  console.log("rendered lobby");
     var room = Rooms.find({});
     room.forEach(function (room) {
-      console.log("people: "+$('.'+room._id).attr("people"));
-      //$('.tooltip-'+room._id).tooltip('hide');
       $('.tooltip-'+room._id).tooltip({trigger:'click'});
 
       //DELETE BUTTON SHOW
       if(room.createdByID === Meteor.userId()){
-        $('.delete-'+room._id).show();
+        $('#delete-'+room._id).show();
       }else{
-        $('.delete-'+room._id).hide();
+        $('#delete-'+room._id).hide();
       }
     });
 }
 
 Template.room.rendered = function(){
-  console.log("rendered room");
-
   //Scroll the msglog All the way to the end
   var elem = document.getElementById('msgLog');
   elem.scrollTop = elem.scrollHeight;
